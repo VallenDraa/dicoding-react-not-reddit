7e9cf07d048074b599f77ed45a78feeb35567d30
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-02-16 22:16:05 +0700

    build: add needed dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// postcss plugins
+import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
+import purgecss from '@fullhuman/postcss-purgecss';
+
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      map: true,
+      plugins: [
+        purgecss({ content: ['./src/**/*.jsx', './index.html'] }),
+        postcssNested,
+        autoprefixer,
+      ],
+    },
+  },
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+});
