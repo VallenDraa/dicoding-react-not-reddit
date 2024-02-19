7fd1f32b70277fb16371dff49ffed6b199f95e20
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-02-19 16:42:58 +0700

    build: move postcss config to its own file

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// postcss plugins
-import autoprefixer from 'autoprefixer';
-import postcssNested from 'postcss-nested';
-import purgecss from '@fullhuman/postcss-purgecss';
-
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      map: true,
-      plugins: [
-        purgecss({ content: ['./src/**/*.jsx', './index.html'] }),
-        postcssNested,
-        autoprefixer,
-      ],
-    },
-  },
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
