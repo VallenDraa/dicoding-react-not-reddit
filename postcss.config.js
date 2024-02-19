d1804bc7a5ca45a520ca0408bbfd6d33e5452d54
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-02-19 17:35:14 +0700

    build: remove purgecss and update css module scoped name

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
 import autoprefixer from 'autoprefixer';
 import postcssNested from 'postcss-nested';
-import purgecss from '@fullhuman/postcss-purgecss';
 
 export default {
-  map: true,
-  plugins: [
-    purgecss({ content: ['./src/**/*.jsx', './index.html'] }),
-    postcssNested,
-    autoprefixer,
-  ],
+  plugins: [postcssNested, autoprefixer],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import postcssNested from 'postcss-nested';
 import react from '@vitejs/plugin-react-swc';
 
 import path from 'path';
@@ -6,6 +7,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: 'not-reddit-[local]_[hash:base64:5]',
+    },
+  },
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
