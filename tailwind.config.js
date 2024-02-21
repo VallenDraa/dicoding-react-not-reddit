e85cfacc663134cf45d34abbb091ddfbcdd1d0c1
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-02-21 20:14:52 +0700

    feat: migrate to tailwind because i'm too lazy to create a design system😅

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import postcssNested from 'postcss-nested';
 import react from '@vitejs/plugin-react-swc';
 
 import path from 'path';
@@ -7,11 +6,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      generateScopedName: 'not-reddit-[local]_[hash:base64:5]',
-    },
-  },
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
