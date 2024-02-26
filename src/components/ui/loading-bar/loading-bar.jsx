9c66d196b041f757e56589b36d0903f9b556d1a2
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-02-26 09:48:21 +0700

    fix: vscode imported the wrong component so i had to debug for 3 hours for this loading bar

--- src/components/ui/loading-bar/loading-bar.jsx
+++ src/components/ui/loading-bar/loading-bar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { LoadingBar } from 'react-redux-loading-bar';
+import LoadingBar from 'react-redux-loading-bar';
 
 export function Loading() {
   return (
     <div className="fixed top-0 z-30 w-full">
-      <LoadingBar />
+      <LoadingBar style={{ height: '5px', background: 'rgb(20 184 166)' }} />
     </div>
   );
 }
